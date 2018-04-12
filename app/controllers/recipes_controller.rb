a1ecf645328037860b847a159e9fe0e7b7e4f964
azeelleeza <leezaluncheon@gmail.com>
2018-04-12 18:59:13 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients << Ingredient.new
+    @recipe.ingredients << Ingredient.new
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+
+    if @recipe.valid?
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+
   end
+
+  private
+
+  def recipe_params
+    # puts params
+    params.require(:recipe).permit(Recipe.column_names,:ingredients_attributes=>[:name, :quantity])
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,17 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    # puts ingredients_attributes
+    self.ingredients << Ingredient.find_or_create_by(
+      name: ingredients_attributes['0']['name'],
+      quantity: ingredients_attributes['0']['quantity']
+    )
+
+    self.ingredients << Ingredient.find_or_create_by(
+      name: ingredients_attributes['1']['name'],
+      quantity: ingredients_attributes['1']['quantity']
+    )
+  end
 end
